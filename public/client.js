96bfc893f622de0ce1afec6d62226bd920b31ab2
moushumi-das <dasm3@myumanitoba.ca>
2021-07-24 15:29:40 -0500

    socket connection

--- public/client.js
+++ public/client.js
@@ -1,17 +1,74 @@
-const e = require("cors");
-
 const socket = io()
+let username;
 let textarea = document.querySelector('#textarea')
-
-let name;
+let messageArea = document.querySelector('.message__area')
 do {
-    name = prompt('please enter your name:')
+    username = prompt('Please enter your name: ')
+} while (!username)
 
-
-} while (!name)
-
-textarea.addEventListener('keyup', () => {
+textarea.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
         sendMessage(e.target.value)
+        console.log(e.target.value)
     }
 })
+
+function sendMessage(message) {
+    let msg = {
+            user: username,
+            message: message.trim()
+        }
+        // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    console.log('Hello!')
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+function sendMessage(message) {
+    let msg = {
+        user: username,
+        message: message
+    }
+    appendMessage(msg, 'outgoing')
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
 app.use(express.static(__dirname + '/public'))
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`)
-)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+// Socket 
 const io = require('socket.io')(http)
+
 io.on('connection', (socket) => {
-    console.log('Connected...')
+    console.log("socket connection successfull" + " " + socket.id)
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
 })
